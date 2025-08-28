00c48c2085b04c7d2ee7ef3d1d001c37aa7631df
bielpieroli <joaogabrielpieroli18@gmail.com>
2025-08-28 18:14:12 -0300

    😀 working

--- SCRIPTS/ORGANIZADOR/organizarPorExtensoes.sh
+++ SCRIPTS/ORGANIZADOR/organizarPorExtensoes.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
 
-# Diretório alvo (se não passar argumento, usa o diretório atual)
 DIR="${1:-.}"
 
-# Verifica se o diretório existe
 if [ ! -d "$DIR" ]; then
     echo "Diretório não encontrado: $DIR"
     exit 1
 fi
 
-# Percorre os arquivos do diretório
 for file in "$DIR"/*; do
     if [ -f "$file" ]; then
-        # Pega a extensão (parte depois do último ponto)
+
         ext="${file##*.}"
         name="${file##*/}"
 
-        # Se não tiver extensão, coloca em "sem_extensao"
         if [ "$name" = "$ext" ]; then
             ext="sem_extensao"
         fi
 
-        # Cria pasta para a extensão, se não existir
+        ext="${ext,,}"
+        
         mkdir -p "$DIR/$ext"
 
-        # Move o arquivo para a pasta correspondente
         mv "$file" "$DIR/$ext/"
     fi
 done
